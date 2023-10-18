5c3f0f13c126e08f48a48f8c762178d144998ef0
pedrocarlo <pedro.muniz.carlo@gmail.com>
2023-10-18 03:23:10 +0000

    commiting to access in hotel

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
-logger = logging.getLogger('uvicorn').getChild('api')
+
+logger = logging.getLogger("uvicorn").getChild("api")
 c_handler = logging.StreamHandler()
-c_handler.setFormatter(logging.Formatter('%(name)s %(levelname)s - %(message)s'))
+c_handler.setFormatter(logging.Formatter("%(name)s %(levelname)s - %(message)s"))
 logger.addHandler(c_handler)
 logger.setLevel(logging.INFO)
 
-logger.info('test')
+logger.info("test")
